344c8592c565fa4f446a65d0567baed8763e4490
Marcus Nebel <marcusnebel09@gmail.com>
2026-03-02 11:48:12 +0000

    NPM package structure

--- android/app/src/main/kotlin/com/example/jimce/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.jimce_music.jimce
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ lib/index.ts
@@ -0,0 +1,3 @@
+const res = await fetch('https://api.deezer.com/search?q=Bella%20Napoli')
+
+console.log(res)
